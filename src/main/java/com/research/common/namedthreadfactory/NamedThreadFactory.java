63f6cb7dc54d4f778a5477bd551d6faff3096dfe
heelenyc <heelenyc@gmail.com>
2014-06-11 14:49:16 +0800

    几个utils

---
+++ src/main/java/com/research/common/namedthreadfactory/NamedThreadFactory.java
@@ -0,0 +1,35 @@
+package com.research.common.namedthreadfactory;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+
+    private final ThreadGroup threadGroup;
+    private final String prefix;
+    private final boolean isDaemon;
+    private final AtomicInteger sequence = new AtomicInteger(1);
+
+    public NamedThreadFactory(String threadNamePrefix) {
+        this(threadNamePrefix, false);
+    }
+
+    public NamedThreadFactory(String threadNamePrefix, boolean isDaemon) {
+        SecurityManager sm = System.getSecurityManager();
+        this.threadGroup = (sm == null) ? Thread.currentThread().getThreadGroup() : sm.getThreadGroup();
+        this.prefix = threadNamePrefix + "-thread-";
+        this.isDaemon = isDaemon;
+    }
+
+    public ThreadGroup getThreadGroup() {
+        return threadGroup;
+    }
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+        String name = prefix + sequence.getAndIncrement();
+        Thread ret = new Thread(threadGroup, runnable, name, 0);
+        ret.setDaemon(isDaemon);
+        return ret;
+    }
+
+}
