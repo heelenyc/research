0afd885591e989a45575508425d885f1718eb23a
heelenyc <heelenyc@gmail.com>
2014-06-25 18:27:07 +0800

    netty study

--- src/main/java/com/research/misc/netty/pingpang/ThroughputMonitor.java
+++ src/main/java/com/research/misc/netty/pingpang/ThroughputMonitor.java
@@ -1,31 +1,30 @@
 package com.research.misc.netty.pingpang;
 
-public class ThroughputMonitor extends Thread {  
-    
-    /** 
-     * Constructor 
-     */  
-    public ThroughputMonitor() {  
-    }  
-  
-    @Override  
-    public void run() {  
-        try {  
-            long oldCounter = PongHandler.getTransferredBytes();  
-            long startTime = System.currentTimeMillis();  
-            for (;;) {  
-                Thread.sleep(3000);  
-  
-                long endTime = System.currentTimeMillis();  
-                long newCounter = PongHandler.getTransferredBytes();  
-                System.err.format("%4.3f MiB/s%n", (newCounter - oldCounter) *  
-                        1000 / (endTime - startTime) / 1048576.0);  
-                oldCounter = newCounter;  
-                startTime = endTime;  
-            }  
-        } catch (InterruptedException e) {  
-            // Stop monitoring asked  
-            return;  
-        }  
-    }  
-} 
+public class ThroughputMonitor extends Thread {
+
+    /**
+     * Constructor
+     */
+    public ThroughputMonitor() {
+    }
+
+    @Override
+    public void run() {
+        try {
+            long oldCounter = PongHandler.getTransferredBytes();
+            long startTime = System.currentTimeMillis();
+            for (;;) {
+                Thread.sleep(3000);
+
+                long endTime = System.currentTimeMillis();
+                long newCounter = PongHandler.getTransferredBytes();
+                System.err.format("%4.3f MiB/s%n", (newCounter - oldCounter) * 1000 / (endTime - startTime) / 1048576.0);
+                oldCounter = newCounter;
+                startTime = endTime;
+            }
+        } catch (InterruptedException e) {
+            // Stop monitoring asked
+            return;
+        }
+    }
+}
